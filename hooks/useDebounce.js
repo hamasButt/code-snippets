0f3b49864017cfdeffe33a7ba8d6d63542b71aa3
Hamas Ahmed <89587893+hamasButt@users.noreply.github.com>
2023-09-04 20:17:34 +0500

    Update useDebounce.js

--- hooks/useDebounce.js
+++ hooks/useDebounce.js
@@ -9,3 +9,22 @@ const useDebounce = (value, delay = 500) => {
 
     return debouncedValue
 }
+
+// funtion for debouncing in a searchcall function using onchange event
+
+  function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+      const later = () => {
+        timeout = null;
+        func(...args);
+      };
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  }
+// how to use it
+  const debouncedSearchCall = debounce(
+    (pageValue, rangeState, value) => facilityTableDataTriggerFunction(pageValue, rangeState, undefined, value),
+    600 // 300ms delay
+  );
