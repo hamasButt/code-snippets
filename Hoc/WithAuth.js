5b04bf2de340e1ccf3fe479bce4a460431bd3c52
Hamas Ahmed <89587893+hamasButt@users.noreply.github.com>
2023-09-14 21:28:20 +0500

    Update WithAuth.js

--- Hoc/WithAuth.js
+++ Hoc/WithAuth.js
@@ -55,20 +55,30 @@ export default withAuth
 
 // IMPORTANT
 
+import { useRouter } from 'next/router'
 const withAuth = (WrappedComponent) => {
     return (props) => {
         // checks whether we are on client / browser or server.
         if (typeof window !== 'undefined') {
             const Router = useRouter()
-
-            const accessToken = localStorage.getItem('Auth')
-
+            const jwt = require('jsonwebtoken');
+            const isAuthenticated = localStorage.getItem('Auth')
+            let user = null
+            let secretKey = 'secretKey'
+            if (typeof isAuthenticated === 'string') {
+                console.log("=>(WithAuth.js:13) isAuthenticated", isAuthenticated);
+                try {
+                    user = jwt.decode(isAuthenticated, secretKey);
+                    // 'decoded' now contains the decoded JWT payload
+                } catch (error) {
+                    console.error('Invalid token:', error);
+                }
+            }
             // If there is no access token we redirect to "/" page.
-            if (!accessToken) {
+            if (!user || user === null) {
                 Router.replace('/')
                 return null
             }
-
             // If this is an accessToken we just render the component that was passed with all its props
 
             return <WrappedComponent {...props} />
@@ -78,3 +88,5 @@ const withAuth = (WrappedComponent) => {
         return null
     }
 }
+
+export default withAuth
