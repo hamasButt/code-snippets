20eae3557954c9fc3bbdf075b5f05cdf0de992fe
Hamas Ahmed <89587893+hamasButt@users.noreply.github.com>
2023-09-14 20:01:02 +0500

    Update WithAuth.js

--- Hoc/WithAuth.js
+++ Hoc/WithAuth.js
@@ -42,3 +42,39 @@ const withAuth = (Component) => {
 };
 
 export default withAuth
+
+
+
+
+
+
+
+
+
+
+
+// IMPORTANT
+
+const withAuth = (WrappedComponent) => {
+    return (props) => {
+        // checks whether we are on client / browser or server.
+        if (typeof window !== 'undefined') {
+            const Router = useRouter()
+
+            const accessToken = localStorage.getItem('Auth')
+
+            // If there is no access token we redirect to "/" page.
+            if (!accessToken) {
+                Router.replace('/')
+                return null
+            }
+
+            // If this is an accessToken we just render the component that was passed with all its props
+
+            return <WrappedComponent {...props} />
+        }
+
+        // If we are on server, return null
+        return null
+    }
+}
